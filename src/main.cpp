e0ed0b6e9e6c9bf1c210b4e304799d5f9b4ce24f
Leo <s80387@beuth-hochschule.de>
2023-08-14 01:28:53 +0200

    glfw window

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,30 @@
 // Created by leona on 8/13/2023.
 //
 #include <cstdio>
+#include <GLFW/glfw3.h>
+
 int main() {
+    GLFWwindow *window;
+
+    if (!glfwInit()) {
+        printf("Couln't initialize GLFW\n");
+        return -1;
+    }
+
+    window = glfwCreateWindow(640, 480, "Hello, World!", NULL, NULL);
+
+    if (!window) {
+        printf("Couldn't create window\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
     printf("Hello, World!");
     return 0;
 }
